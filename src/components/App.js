f5e4e60ce4ba604477c2db88d84b56218d92b0e8
Gaurav Maheshwari <gauravmaheshwari57@gmail.com>
2023-10-14 18:55:01 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="container"></div>
+      <Footer />
+    </div>
+  );
+}
